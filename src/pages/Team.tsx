a85b7389f198c732943723d53fbf76e4b7b88544
Yousuf2110 <qaziyousuf701@gmail.com>
2025-07-18 16:04:20 +0500

    fixed

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -523,7 +523,6 @@ export default function Team() {
             </div>
           </div>
 
-          {/* Team Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {filteredEmployees.map((employee, index) => (
               <div
